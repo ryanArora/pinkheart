0310469e68c255d59de551376dbe8e2d24177353
Ryan Arora <arora.ryan@gmail.com>
2022-01-08 00:44:55 +0000

    Update index.js

--- index.js
+++ index.js
@@ -2,24 +2,7 @@
 
 // for ava <3
 
-const fs = require("fs");
-const path = require("path");
 const colors = require("colors");
-
-const TXT_FILES = "txts";
-
-// const files = fs.readdirSync(TXT_FILES);
-// files.sort((a, b) => {
-//   a = path.basename(a, ".txt");
-//   b = path.basename(b, ".txt");
-//   return parseInt(a) - parseInt(b)
-// });
-
-// const txts = files.map((fileName) => {
-//   const pth = path.join(TXT_FILES, fileName);
-//   const file = fs.readFileSync(pth);
-//   return colors.magenta(file.toString());
-// });
 
 let txts = [
   '                                                \n' +
@@ -262,7 +245,7 @@ let txts = [
   '                              WNKXW             \n' +
   '                                                \n' +
   '                                                \n'
-]
+];
 
 txts = txts.map((str) => colors.magenta(str));
 
